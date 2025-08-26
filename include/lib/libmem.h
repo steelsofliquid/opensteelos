958db5dae13cf820d8f3f93e730ba461cd563134
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2025-08-25 22:15:02 -0400

    Added placeholders for library header files
    These have no errors, but nothing in them either. The next batch of builds (0.22.40 through to 0.22.??) will include these libraries because there's no access to standard C or C++ libraries.

---
+++ include/lib/libmem.h
@@ -0,0 +1,19 @@
+
+#ifndef __OSOS__LIB__LIBMEM_H
+#define __OSOS__LIB__LIBMEM_H
+
+namespace osos
+{
+    namespace libs
+    {
+        class MemoryLibrary
+        {
+
+        public:
+        MemoryLibrary();
+        ~MemoryLibrary();
+        };
+    }
+}
+
+#endif
---
+++ include/lib/libstr.h
@@ -0,0 +1,19 @@
+
+#ifndef __OSOS__LIB__LIBSTR_H
+#define __OSOS__LIB__LIBSTR_H
+
+namespace osos
+{
+    namespace libs
+    {
+        class StringLibrary
+        {
+
+        public:
+        StringLibrary();
+        ~StringLibrary();
+        };
+    }
+}
+
+#endif
