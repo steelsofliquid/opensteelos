a4a5ec856f4251d72f44a0451c340c6a8446acf7
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2025-08-27 14:35:18 -0400

    Updated libmem in provisional

--- include/lib/libmem.h
+++ include/lib/libmem.h
@@ -2,18 +2,25 @@
 #ifndef __OSOS__LIB__LIBMEM_H
 #define __OSOS__LIB__LIBMEM_H
 
-namespace osos
-{
-    namespace libs
-    {
-        class MemoryLibrary
-        {
+    #include <common/types.h>
 
-        public:
-        MemoryLibrary();
-        ~MemoryLibrary();
-        };
+    namespace osos
+    {
+        namespace libs
+        {
+            class MemoryLibrary
+            {
+
+            public:
+            MemoryLibrary();
+            ~MemoryLibrary();
+
+            void* memcpy(void* destination, const void* source, osos::common::size_t number);
+            void* memmove(void* destination, const void* source, osos::common::size_t number);
+            void* memset(void* destination, osos::common::int32_t value, osos::common::size_t number);
+            osos::common::uint32_t memcmp(const void* size1, const void* size2, osos::common::size_t number);
+            };
+        }
     }
-}
 
 #endif
