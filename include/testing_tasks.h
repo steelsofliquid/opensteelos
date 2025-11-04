9da3dcea7a5d1aaedd42aeeca4997f87b9302c6b
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2025-11-04 13:07:35 -0500

    Add files via upload

---
+++ include/testing_tasks.h
@@ -0,0 +1,20 @@
+#ifndef __TESTING_TASKS_H
+#define __TESTING_TASKS_H
+
+#include <common/types.h>
+#include <multitasking.h>
+#include <drivers/pit.h>
+
+namespace osos
+{
+    void TestTask1();
+    void TestTask2();
+    void TestTask3();
+
+    void TestTask4();
+    void TestTask5();
+    void TestTask6();
+    void TestTask7();
+}
+
+#endif
