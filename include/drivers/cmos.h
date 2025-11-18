b21f07aa98c07426144f6044f6aecec2d813454a
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2025-11-18 11:43:04 -0500

    Added updated CMOS header

--- include/drivers/cmos.h
+++ include/drivers/cmos.h
@@ -3,53 +3,57 @@
 #ifndef __OSOS__DRIVERS__CMOS_H
 #define __OSOS__DRIVERS__CMOS_H
 
-    #include <common/types.h>
-    #include <drivers/driver.h>
-    #include <hwcom/port.h>
-    #include <hwcom/interrupts.h>
+#include <common/types.h>
+#include <drivers/driver.h>
+#include <hwcom/port.h>
+#include <hwcom/interrupts.h>
+#include <globalfuncs.h>
 
-    namespace osos
+namespace osos
+{
+    namespace drivers
     {
-        namespace drivers
+
+        struct RealTimeClockRegisters
         {
-            class ClockBatteryDriver
-            {
+            osos::common::uint8_t second;
+            osos::common::uint8_t minute;
+            osos::common::uint8_t hour;
+            osos::common::uint8_t day;
+            osos::common::uint8_t month;
+            osos::common::uint32_t year;
+        };
+
+        class ClockBatteryDriver
+        {
             
-                struct RealTimeClockRegisters
-                {
-                    osos::common::uint8_t second;
-                    osos::common::uint8_t minute;
-                    osos::common::uint8_t hour;
-                    osos::common::uint8_t day;
-                    osos::common::uint8_t month;
-                    osos::common::uint32_t year;
-                };
 
-                private:
-                void GetIndex(osos::common::uint8_t register);
-                void ReadData();
-                void WriteData(osos::common::uint8_t value);
 
-                public:
-                osos::hwcom::Port8Bit portAddressCMOS;
-                osos::hwcom::Port8Bit portDataCMOS;
+            private:
+            void GetIndex(osos::common::uint8_t reg);
+            void ReadData();
+            void WriteData(osos::common::uint8_t value);
 
-                public:
-                ClockBatteryDriver();
-                ~ClockBatteryDriver();
+            public:
+            osos::hwcom::Port8Bit portAddressCMOS;
+            osos::hwcom::Port8Bit portDataCMOS;
 
-                void ReadCMOS(osos::common::int32_t port);
-                int WriteCMOS(osos::common::int32_t port, osos::common::int32_t value);
+            public:
+            ClockBatteryDriver();
+            ~ClockBatteryDriver();
 
-                // these functions are for the realtime clock
-                int GetProgressUpdateFlag();
-                osos::common::uint8_t GetRTCregister(osos::common::int32_t register);
-                void ReadRTC();
-                void GrabSystemTime();
+            osos::common::uint8_t ReadCMOS(osos::common::int32_t port);
+            void WriteCMOS(osos::common::int32_t port, osos::common::int32_t value);
 
-                // We do not need a HandleInterrupt(uint32_t esp) for this driver.
-            };
-        }
+            // these functions are for the realtime clock
+            int GetProgressUpdateFlag();
+            osos::common::uint8_t GetRTCregister(osos::common::int32_t reg);
+            RealTimeClockRegisters ReadRTC();
+            // void GrabSystemTime();
+
+            // We do not need a HandleInterrupt(uint32_t esp) for this driver.
+        };
     }
+}
 
 #endif
