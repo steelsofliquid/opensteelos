826ecd5edfa9890950c5aa82893e0f6a273d966c
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2025-11-25 15:34:49 -0500

    Added version.h header
    This way there's not a bajillion extern int32_t bits all over

---
+++ include/common/version.h
@@ -0,0 +1,11 @@
+// OpenSteel/OS Version Headers API
+#ifndef __OSOS__COMMON__VERSION_H
+#define __OSOS__COMMON__VERSION_H
+
+#include <common/types.h>
+
+extern osos::common::int32_t verMajor;
+extern osos::common::int32_t verMinor;
+extern osos::common::int32_t verBuild;
+
+#endif
