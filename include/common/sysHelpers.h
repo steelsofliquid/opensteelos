96fdf982ea5fdf52b650d9d2ba976984b38e0393
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2025-11-27 17:28:53 -0500

    Added an assistance header, sysHelpers.h
    This is to provide a global mode for different UIs. More content will be added to this at a later date, if needed.

---
+++ include/common/sysHelpers.h
@@ -0,0 +1,14 @@
+#ifndef __OSOS__COMMON__SYSHELPERS_H
+#define __OSOS__COMMON__SYSHELPERS_H
+
+enum InterfaceModes
+{
+    BootUI         = 0,
+    LogonUI        = 1,
+    TextShell      = 2,
+    GraphicShell   = 3,
+    TextModeEditor = 4,
+    TextModeOther  = 5
+};
+
+#endif
