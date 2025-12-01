d04e7f97bca831aa5a1a9924b62ff62f7eb2d383
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2025-12-01 16:46:22 -0500

    Updated speaker driver header to comply with styleguide

--- include/drivers/snd/speaker.h
+++ include/drivers/snd/speaker.h
@@ -12,28 +12,28 @@ namespace osos
         class Speaker
         {
             public:
-                osos::hwcom::Port8Bit PITChannel2;
-                osos::hwcom::Port8Bit PITCom;
-                osos::hwcom::Port8Bit SpeakerPort;
+            osos::hwcom::Port8Bit PITChannel2;
+            osos::hwcom::Port8Bit PITCom;
+            osos::hwcom::Port8Bit SpeakerPort;
 
             public:
-                Speaker();
-                ~Speaker();
+            Speaker();
+            ~Speaker();
 
-                struct meledy
-                {
-                    osos::common::uint32_t frequency;
-                    osos::common::uint32_t length;
-                };
+            struct meledy
+            {
+                osos::common::uint32_t frequency;
+                osos::common::uint32_t length;
+            };
                 
-                // hardware functions
-                void play_sound(osos::common::uint32_t nFrequence);
-                void nosound();
+            // hardware functions
+            void play_sound(osos::common::uint32_t nFrequence);
+            void nosound();
 
-                // system sounds (might be transferred to their own object in the future)
-                void BeepVariable(osos::common::uint32_t frequency, osos::common::uint32_t interval);
-                void BeepBasic();
-                void LifeChime();
+            // system sounds (might be transferred to their own object in the future)
+            void BeepVariable(osos::common::uint32_t frequency, osos::common::uint32_t interval);
+            void BeepBasic();
+            void LifeChime();
         };
     }
 }
