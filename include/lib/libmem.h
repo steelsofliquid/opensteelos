8863d4099a5e1f5a6c8ca3060093f19fa954fc43
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2025-12-01 16:50:40 -0500

    Updated libmem and libstr headers
    For libmem, its more styleguide compliance updates.
    For libstr, its the addition of a to-be implementation of strtok.

--- include/lib/libmem.h
+++ include/lib/libmem.h
@@ -2,17 +2,17 @@
 #ifndef __OSOS__LIB__LIBMEM_H
 #define __OSOS__LIB__LIBMEM_H
 
-    #include <common/types.h>
+#include <common/types.h>
 
-    namespace osos
+namespace osos
+{
+    namespace libs
     {
-        namespace libs
-        {
-            void* memcpy(void* destination, const void* source, osos::common::size_t number);
-            void* memmove(void* destination, const void* source, osos::common::size_t number);
-            void* memset(void* destination, osos::common::int32_t value, osos::common::size_t number);
-            osos::common::uint32_t memcmp(const void* size1, const void* size2, osos::common::size_t number);
-        }
+        void* memcpy(void* destination, const void* source, osos::common::size_t number);
+        void* memmove(void* destination, const void* source, osos::common::size_t number);
+        void* memset(void* destination, osos::common::int32_t value, osos::common::size_t number);
+        osos::common::uint32_t memcmp(const void* size1, const void* size2, osos::common::size_t number);
     }
+}
 
 #endif
--- include/lib/libstr.h
+++ include/lib/libstr.h
@@ -2,26 +2,28 @@
 #ifndef __OSOS__LIB__LIBSTR_H
 #define __OSOS__LIB__LIBSTR_H
 
-    #include <common/types.h>
+#include <common/types.h>
 
-    namespace osos
+namespace osos
+{
+    namespace libs
     {
-        namespace libs
-        {
-            osos::common::size_t strlen(const char* string);
-            osos::common::int32_t strcmp(const char* string1, const char* string2);
-            osos::common::int32_t strncmp(const char* string1, const char* string2, osos::common::size_t n);
+        osos::common::size_t strlen(const char* string);
+        osos::common::int32_t strcmp(const char* string1, const char* string2);
+        osos::common::int32_t strncmp(const char* string1, const char* string2, osos::common::size_t n);
 
-            osos::common::int8_t* strcpy(char* destination, const char* source);
-            osos::common::int8_t* strncpy(char* destination, const char* source, osos::common::size_t n);
-            osos::common::int8_t* strcat(char* destination, const char* source);
+        osos::common::int8_t* strcpy(char* destination, const char* source);
+        osos::common::int8_t* strncpy(char* destination, const char* source, osos::common::size_t n);
+        osos::common::int8_t* strcat(char* destination, const char* source);
 
-            osos::common::int8_t* strchr(const char* string, osos::common::int32_t character);
-            osos::common::int8_t* strrchr(const char* string, osos::common::int32_t character);
-            osos::common::int8_t* strstr(const char* haystack, const char* needle);
+        osos::common::int8_t* strchr(const char* string, osos::common::int32_t character);
+        osos::common::int8_t* strrchr(const char* string, osos::common::int32_t character);
+        osos::common::int8_t* strstr(const char* haystack, const char* needle);
 
-            osos::common::int8_t* itoa(osos::common::int32_t value, char* string, osos::common::int32_t base);
-        }
+        osos::common::int8_t* itoa(osos::common::int32_t value, char* string, osos::common::int32_t base);
+
+        osos::common::int8_t* strtok(char* newstr, const char* delim);
     }
+}
 
 #endif
