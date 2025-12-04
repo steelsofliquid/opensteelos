64face92069063240c9e97a0f9ebbb55a8473554
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2025-12-04 15:18:24 -0500

    Updated NRSh commands header
     - Added placeholder command for a later-date package manager.
     - Considered adding to a couple commands, but there is no interface which makes adding either clock or beep feasible right now.

--- include/cliCommands.h
+++ include/cliCommands.h
@@ -3,7 +3,9 @@
 
 #include <common/types.h>
 #include <common/version.h>
+#include <drivers/snd/speaker.h>
 #include <drivers/pit.h>
+#include <drivers/rs232.h>
 #include <lib/libcpu.h>
 #include <globalfuncs.h>
 
@@ -18,11 +20,12 @@ namespace osos
     void cmdCpudata(osos::common::uint32_t argc, const char** argv);
     void cmdUptime(osos::common::uint32_t argc, const char** argv);
     void cmdShutdown(osos::common::uint32_t agrc, const char** argv);
+    void cmdPacinae(osos::common::uint32_t argc, const char** argv);
 
     void cmdEcho(osos::common::uint32_t argc, const char** argv);
     void cmdCls(osos::common::uint32_t argc, const char** argv);
-    //void cmdClock(osos::common::uint32_t agrc, const char* argv);
-    //void cmdBeep(int agrc, const char* argv);
+    //void cmdClock(osos::common::uint32_t agrc, const char** argv);
+    //void cmdBeep(osos::common::uint32_t agrc, const char** argv);
     
     void cmdVer(osos::common::uint32_t argc, const char** argv);
     void cmdHelp(osos::common::uint32_t argc, const char** argv);
