8249e1ae2304beb5398c11b25b65a5eed207e3df
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2025-12-19 14:00:27 -0500

    Added storage medium driver headers
    atapi.h and fdc.h are useless, in order to get the directory in one commit I need to upload the whole directory

---
+++ include/drivers/drv/ata.h
@@ -0,0 +1,50 @@
+#ifndef __OSOS__DRIVERS__DRV__ATA_H
+#define __OSOS__DRIVERS__DRV__ATA_H
+
+#include <common/types.h>
+#include <drivers/rs232.h>
+#include <kernel/hwcom/port.h>
+
+namespace osos
+{
+    namespace drivers
+    {
+        namespace drives
+        {
+            class AdvancedTechnologyAttachment
+            {
+                protected:
+                osos::kernel::hwcom::Port16Bit dataPort;
+                osos::kernel::hwcom::Port8Bit  errorPort;
+                osos::kernel::hwcom::Port8Bit  sectorCountPort;
+                osos::kernel::hwcom::Port8Bit  lbaLowPort;
+                osos::kernel::hwcom::Port8Bit  lbaMidPort;
+                osos::kernel::hwcom::Port8Bit  lbaHighPort;
+                osos::kernel::hwcom::Port8Bit  devicePort;
+                osos::kernel::hwcom::Port8Bit  commandPort;
+                osos::kernel::hwcom::Port8Bit  controlPort;
+
+                // This terminology would've traditionally been called "master" and "sl*ve", as part of that
+                // terminology, but I said it once (in pic.cpp) and I'll say it again here:
+                // That terminology is heavily outdated. I'd rather not get into this rabbit hole.
+                // As such, we use "lead" and "follow".
+                bool lead;
+
+                osos::common::uint16_t bytesPerSector;
+
+                public:
+                AdvancedTechnologyAttachment(osos::common::uint16_t portBase, bool lead);
+                ~AdvancedTechnologyAttachment();
+
+                void IdentifyDrive();
+                void Read28Bit(osos::common::uint32_t sector, char* data, int count);
+                void Write28Bit(osos::common::uint32_t sector, char* data, int count);
+                void FlushDrive(); // Flush>Drive< to differentiate from kernel-NRSh function Flush>Shell<.
+
+                // TODO one day: Add a twin-journal system
+            };
+        }
+    }
+}
+
+#endif
