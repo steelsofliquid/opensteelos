e1a6a52b14eca1ad66c095750358e5bb81ae5f51
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2025-12-30 13:00:21 -0500

    Updated globalfuncs.h

--- include/globalfuncs.h
+++ include/globalfuncs.h
@@ -3,9 +3,8 @@
 
 #include <common/types.h>
 #include <common/version.h>
-#include <drivers/cmos.h>
 // #include <drivers/pit.h>
-#include <userland/lib/libstr.h>
+#include <common/lib/libstr.h>
 
 namespace osos
 {
@@ -15,31 +14,10 @@ namespace osos
     // --- system APIs ---
     // GrabSys APIs
     // DO NOT USE!!
-    osos::common::int8_t* GrabSysVer(osos::common::int8_t sector);
+    int8_t* GrabSysVer(int8_t sector);
 
     // hardware materials
-    void HardSleep(osos::common::uint32_t interval);
-
-
-    // inline assembly carriers
-    static inline void outb(osos::common::uint16_t port, osos::common::uint8_t value)
-    {
-        asm volatile("outb %0, %1" : : "a" (value), "Nd" (port));
-    }
-    static inline osos::common::uint8_t inb(osos::common::uint16_t port)
-    {
-        osos::common::uint8_t result;
-        asm volatile("inb %1, %0" : "=a" (result) : "Nd" (port));
-        return result;
-    }
-    static inline void cpuid(int code, 
-        osos::common::uint32_t* a, 
-        osos::common::uint32_t* b, 
-        osos::common::uint32_t* c, 
-        osos::common::uint32_t* d)
-    {
-        asm volatile("cpuid" : "=a"(*a), "=b"(*b), "=c"(*c), "=d"(*d) : "a"(code), "c"(0));
-    }
+    void HardSleep(uint32_t interval);
 }
 
 #endif
