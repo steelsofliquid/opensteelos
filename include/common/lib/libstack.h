dd367dae9705099c25841933258454941f1f5e60
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2026-02-02 16:45:54 -0500

    Added libstack.h from 0.22.305
    Wasn't included in anything in 0.22.305, placed here for simplicity

---
+++ include/common/lib/libstack.h
@@ -0,0 +1,11 @@
+#ifndef __OSOS__COMMON__LIB__LIBSTACK_H
+#define __OSOS__COMMON__LIB__LIBSTACK_H
+
+#include <common/types.h>
+#include <kernel/mem/dmm.h>
+
+// This library (libstack.h) defines a sort of pseudo-type. Stacks are memory data structures (for instance, an array or list stored in memory) and so putting its functions in a base unclassified library is less than ideal.
+class Stack
+{};
+
+#endif
