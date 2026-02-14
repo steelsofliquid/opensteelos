40eba8bdf4e5745d17f447184b4643d0bba6cad4
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2026-02-13 22:24:29 -0500

    Updated driverManager.h - Addition of activeDriverManager
    This may need further adjustment to accommodate formal shutdown procedures that need to be worked on as a result of an influx of new features.

--- include/kernel/hwcom/driverManager.h
+++ include/kernel/hwcom/driverManager.h
@@ -19,6 +19,8 @@ namespace osos
                 int32_t driverCount;
 
                 public:
+                static DriverManager *activeDriverManager;
+                
                 DriverManager();
                 ~DriverManager();
 
