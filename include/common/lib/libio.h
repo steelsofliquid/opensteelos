769c44ba013563353611d610fbef6b7f972d2c9c
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2026-02-20 18:21:29 -0500

    Updated libio.h library header
    *** View previous commit message for full detail on this build's changes to libio. ***
    
    Changes to libio.h header:
     - Added inclusion of libasm for cursor functionality
     - Added new functions' cutouts defined in libio.cpp.

--- include/common/lib/libio.h
+++ include/common/lib/libio.h
@@ -2,8 +2,30 @@
 #define __OSOS__COMMON__LIB__LIBIO_H
 
 #include <common/types.h>
+#include <common/lib/libasm.h>
 #include <common/lib/libstr.h>
 
+// Helpers for printing functions
+
+void VerifyXY(); // If x and y over the desired limit, new line and/or scroll
+int32_t VerifyPrintProgress(const char* functionInUse); // Better off an int than a bool
+
+
+// Printing things
+
+void putchar(uint8_t character); // wrapper for printf %s and %d and whatnot
 void printf(char* str, ...); // the main screen output function.
+
+
+// Cursor things
+
+void EnableCursor(uint8_t start, uint8_t end);
+void DisableCursor();
+void UpdateCursor();
+
+
+// Key functions
+
+void FlushShell();
 
 #endif
