db2e485517d0e9d7dca731dc84b535be87f02422
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2026-02-20 18:25:27 -0500

    Added placeholder Intel i8042 PS/2 controller hwcom header
    I should state that with devices like the PIC and PS/2 controller, they are located in kernel/hwcom instead of in drivers/ as they are designed to work with devices that have their own drivers and whatnot. I don't know what point I was trying to make here in the moment, but I should get this stuff committed so that I can get out of my warm blankets that'll make me crash before even 19 o'clock.

---
+++ include/kernel/hwcom/i8042.h
@@ -0,0 +1,28 @@
+// Intel i8042 (PS/2 controller) code
+// This file is for consolidating some of the code for the PS/2
+// keyboard and mouse code.
+
+// For context, PS/2 refers to the IBM Personal System/2 line of computers,
+// not the Sony PlayStation 2 (which would be abbreviated as PS2). The
+// Personal System/2 (or PS/2 for short) is where the keyboard and mouse
+// ports in question originated from, hence their namesake.
+
+#ifndef __OSOS__KERNEL__HWCOM__I8042_H
+#define __OSOS__KERNEL__HWCOM__I8042_H
+
+#include <common/types.h>
+#include <kernel/hwcom/interrupts.h>
+#include <kernel/hwcom/port.h>
+
+namespace osos
+{
+    namespace kernel
+    {
+        namespace hwcom
+        {
+            //
+        }
+    }
+}
+
+#endif
