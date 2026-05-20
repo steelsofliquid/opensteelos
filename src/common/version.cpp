c1865f7430c25efb5ab0396db32f2c39377d5b80
SteelsOfLiquid <128747093+steelsofliquid@users.noreply.github.com>
2026-05-20 13:14:57 -0400

    Updated version data
    0.22.395 -> 0.22.399
    
    2026-02-20 -> 2026-05-20

--- src/common/version.cpp
+++ src/common/version.cpp
@@ -2,8 +2,8 @@
 
 int32_t verMajor = 0;
 int32_t verMinor = 22;
-int32_t verBuild = 395;
+int32_t verBuild = 399;
 
 int32_t buildYr  = 2026;
-int32_t buildMon = 2;
+int32_t buildMon = 5;
 int32_t buildDay = 20;
